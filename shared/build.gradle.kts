ee0c5ba482783f82efca1842a3ab7708a97ef7e3
Marwan Aziz <marwan@marwanaziz.net>
2026-04-06 12:28:24 +0100

    publish the project on maven

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.mavenPublish)
 }
 
 fun loadCityScoutSecret(envName: String, propertyName: String): String {
